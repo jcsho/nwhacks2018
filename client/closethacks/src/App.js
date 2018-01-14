ec70f058d2e8db061fd0dbe7a433bbc61aaf8627
takejaw6 <ty.jaw6@gmail.com>
2018-01-13 17:16:35 -0800

    in progress

--- client/closethacks/src/App.js
+++ client/closethacks/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Carousel from 'nuka-carousel';
+import './styles/carousel.css';
+
+const img1 = require('./img/pants_1.jpg');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="headContainer">
+        </div>
+        <Carousel id="topCarousel">
+          <img src={img1} />
+          <img src={img1} />
+          <img src={img1} />
+        </Carousel>
+        <Carousel id="bottomCarousel">
+          <img src={img1} />
+          <img src={img1} />
+          <img src={img1} />
+        </Carousel>
+        <Carousel id="shoeCarousel">
+          <img src={img1} />
+          <img src={img1} />
+          <img src={img1} />
+        </Carousel>
       </div>
     );
   }
