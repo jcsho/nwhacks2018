571919372053022fe14253b0fa33ed17bc97907c
Justin Ho <justinhodev@gmail.com>
2018-01-13 21:02:44 -0800

    Router
    added closet path to vue router

--- client/closethacks/src/router/index.js
+++ client/closethacks/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Closet from '@/components/Closet'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/closet',
+      name: 'Closet',
+      component: Closet
     }
   ]
 })
